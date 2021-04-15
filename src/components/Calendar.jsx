74c30507b3027e6ca5c561ae564bc7f7c98abdf4
Evgeny <48516892+Parmon101@users.noreply.github.com>
2021-04-15 17:27:01 +0300

    Add files via upload

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react'
 import "./Calendar.css";
+import dayjs from 'dayjs'
+
 import Days from './func/days'
 import Header from './func/Header'
 import Cells from './func/Cells'
-import dayjs from 'dayjs'
-
-import Counter from './func/test'
 
 
 function Calendar() {
-  let currentMonth = dayjs()
+  const currentMonth = dayjs()
 
   const [count, setCount] = useState(currentMonth);
 
   return (
     <div className='calendar'> 
-      {Counter(count, setCount)}
       {Header(count, setCount)}
       {Days(count, setCount)}
       {Cells(count, setCount)}
