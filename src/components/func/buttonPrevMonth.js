d8b0444540d37df6279693091bf036d8215e64bc
Evgeny <48516892+Parmon101@users.noreply.github.com>
2021-04-15 17:25:27 +0300

    Delete buttonPrevMonth.js

--- src/components/func/buttonPrevMonth.js
+++
@@ -1,9 +0,0 @@
-import {state} from './../Calendar'
-
-function buttonPrevMonth(e) {
-  // e.preventDefault();
-  state.currentMonth = state.currentMonth.subtract(1, 'month')
-  console.log(state.currentMonth.format('DD MM YYYY'));
-}
-
-export default buttonPrevMonth
