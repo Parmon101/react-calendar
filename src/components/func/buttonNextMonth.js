8702048fc4085408090a761fae9aeadbf433145b
Evgeny <48516892+Parmon101@users.noreply.github.com>
2021-04-15 17:25:21 +0300

    Delete buttonNextMonth.js

--- src/components/func/buttonNextMonth.js
+++
@@ -1,9 +0,0 @@
-import {state} from './../Calendar'
-
-function buttonNextMonth(e) {
-  // e.preventDefault();
-  state.currentMonth = state.currentMonth.add(1, 'month')
-  console.log(state.currentMonth);
-}
-
-export default buttonNextMonth
